9182db31b1cc1c58fbb5db517c1624212e50a78f
metalchesshead <59069601+metalchesshead@users.noreply.github.com>
2026-05-21 13:06:32 -0400

    Update paint.js

--- paint.js
+++ paint.js
@@ -2,11 +2,7 @@ const board = document.getElementById("board");
 const context = board.getContext("2d");
 
 let isDrawing = false;
-const colorPicker = document.getElementById("color-picker");
-const brushSize = document.getElementById("brush-size");
 const clearButton = document.getElementById("clear-button");
-const fillButton = document.getElementById("fill-button");
-const downloadButton = document.getElementById("download-button");
 
 board.addEventListener("pointerdown", () => {isDrawing = true}); //start drawing
 board.addEventListener("pointerup", () => { //stop drawing
@@ -17,9 +13,7 @@ board.addEventListener("pointerout", () => {isDrawing = false}); //mouse leaves 
 board.addEventListener("pointermove", draw);
 board.style.touchAction = "none";
 
-clearButton.addEventListener("click", clearCanvas);
-fillButton.addEventListener("click", fillCanvas);
-downloadButton.addEventListener("click", downloadImage);
+
 
 function draw(e) { //e = event, contains mouse click, position, etc information
     if (!isDrawing) return;
@@ -41,11 +35,4 @@ function clearCanvas() {
 function fillCanvas() {
     context.fillStyle = colorPicker.value;
     context.fillRect(0, 0, board.width, board.height);
-}
-
-function downloadImage() {
-    const imageLink = document.createElement("a");
-    imageLink.download = `kennyyipcoding-${Date.now()}.png`;  //file name
-    imageLink.href = board.toDataURL("image/png");
-    imageLink.click();
 }
