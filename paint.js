6b26a8298af658cf6c7d2253adb1054e12b2f6dc
metalchesshead <59069601+metalchesshead@users.noreply.github.com>
2026-05-21 12:54:11 -0400

    Update paint.js

--- paint.js
+++ paint.js
@@ -1,15 +1,16 @@
 const board = document.getElementById("board")'
 const context = board.getContext("2d");
 
-//const brushSize = document.getElementById("brush-size");
+const brushSize = document.getElementById("brush-size");
 const clearButton = document.getElementById("clear-button");
+const colorPicker = document.getElementById("color-picker");
 
 board.addEventListener("mousemove", draw);
 
 function draw(e) {
-  context.lineWidth = 10px;
+  context.lineWidth = brushSize.vlaue;
   context.lineCap = "round";
-  context.strokeStyle = #000000;
+  context.strokeStyle = colorPicker.value;
 
   context.lineTo(e.offsetX, e.offsetY);
   context.stroke();
