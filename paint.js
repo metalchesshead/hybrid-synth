da1ede0abf371cb5b1345898db0f0a5e9e3c0d94
metalchesshead <59069601+metalchesshead@users.noreply.github.com>
2026-05-21 12:45:55 -0400

    Create paint.js

---
+++ paint.js
@@ -0,0 +1,17 @@
+const board = document.getElementById("board")'
+const context = board.getContext("2d");
+
+//const brushSize = document.getElementById("brush-size");
+const clearButton = document.getElementById("clear-button");
+
+board.addEventListener("mousemove", draw);
+
+function draw() {
+  context.lineCap = "round";
+  context.strokeStyle = colorPicker.value;
+
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+  context.beginPath();
+  context.moveTo(e.offsetX, e.offsetY);
+    }
